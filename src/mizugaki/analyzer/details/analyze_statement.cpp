b99cc5ae002b7cd8c86945f54193908f5e84a26b
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2024-06-18 09:45:26 +0900

    wip: better mocking

--- src/mizugaki/analyzer/details/analyze_statement.cpp
+++ src/mizugaki/analyzer/details/analyze_statement.cpp
@@ -1,13 +1,26 @@
 #include <mizugaki/analyzer/details/analyze_statement.h>
 
+#include <takatori/util/string_builder.h>
+
 namespace mizugaki::analyzer::details {
+
+using ::takatori::util::string_builder;
+
+using diagnostic_type = analyzer_context::diagnostic_type;
+using diagnostic_code = diagnostic_type::code_type;
 
 analyze_statement_result_type analyze_statement(
         analyzer_context& context,
         ast::statement::statement const& statement) {
     // FIXME: mock
-    (void) context;
-    (void) statement;
+
+    context.report(
+            diagnostic_code::unsupported_feature,
+            string_builder {}
+                << "unknown statement: "
+                << statement.node_kind()
+                << string_builder::to_string,
+            statement.region());
     return {};
 }
 
