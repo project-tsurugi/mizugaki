2f4b523db80fe31b7b52668d2b88abe10b3eba0c
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2024-07-08 10:35:57 +0900

    misc: revise examples/parser-cli.

--- examples/parser-cli/main.cpp
+++ examples/parser-cli/main.cpp
@@ -1,5 +1,6 @@
 #include <gflags/gflags.h>
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <string_view>
@@ -33,16 +34,32 @@ using namespace mizugaki::examples::parser_cli;
 DEFINE_bool(quiet, false, "only show diagnostic erroneous"); // NOLINT
 DEFINE_int32(debug, 0, "parser debug level"); // NOLINT
 DEFINE_uint32(repeat, 1, "repeat parse operation"); // NOLINT
+DEFINE_string(file, "", "input file path"); // NOLINT
+DEFINE_string(text, "", "input text"); // NOLINT
 
 int main(int argc, char* argv[]) {
     gflags::SetUsageMessage("mizugaki SQL parser CLI");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
-    if (argc != 2) {
+
+    if ((FLAGS_text.empty() && FLAGS_file.empty()) || (!FLAGS_text.empty() && !FLAGS_file.empty())) {
         gflags::ShowUsageWithFlags(argv[0]); // NOLINT
-        return -1;
+        return 1;
     }
 
-    std::string_view source { argv[1] }; // NOLINT
+    std::string source;
+    if (!FLAGS_text.empty()) {
+        source = FLAGS_text;
+    } else {
+        std::ifstream ifs { FLAGS_file };
+        if (ifs.fail()) {
+            std::cerr << "failed to open file: " << FLAGS_file << std::endl;
+            return 2;
+        }
+        source.assign(
+                std::istreambuf_iterator<char> { ifs },
+                std::istreambuf_iterator<char> {});
+        ifs.close();
+    }
     if (run(source, FLAGS_repeat, FLAGS_quiet, FLAGS_debug)) {
         return 0;
     }
